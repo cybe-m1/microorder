6943c9fb95c57088c0c8d539c5486d3a10fcebae
anthony <quereantho@gmail.com>
2022-02-15 15:40:35 +0100

    Init database

---
+++ customer/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+-- Here write initialization data
+
+INSERT INTO t_customer (id, first_name, last_name, phone_number)
+VALUES (1, 'Artur', 'Pendragon', '0123456789'),
+       (2, 'Léodagan', 'De Carmélide', '1234567890'),
+       (3, 'Perceval', 'De Galle', '2345678901'),
+       (4, 'Karadoc', 'De Vanne', '3456789012'),
+       (5, 'Lancelot', 'Du Lac', '4567890123'),
+       (6, 'Guenièvre', 'De Carmélide', '5678901234'),
+       (7, 'Bohort', 'De Gaune', '6789012345'),
+       (8, 'Le père', 'Blaise', '7890123456'),
+       (9, 'Séli', 'De Carmélide', '8901234567'),
+       (10, 'Yvain', 'Chevalier au Lion', '9012345678');
+
